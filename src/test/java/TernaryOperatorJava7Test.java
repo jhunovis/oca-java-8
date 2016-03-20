49d4b1ef2b113449c89ee1d8a6fae487fd4856e4
Jan Hackel <jhunovis+github@googlemail.com>
2016-03-20 09:45:19 +0100

    Add test-case for ternary operator behavior with different expression result types.
    This from chapter two, page 117 of the "OCA: Oracle® Certified Associate
    Java® SE 8 Programmer I" study guide.

--- src/test/java/TernaryOperatorJava7Test.java
+++ src/test/java/TernaryOperatorJava7Test.java
@@ -4,23 +4,27 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * Preparing for the Oracle Certified Associate Java SE 8 Programmer 1 exam, I came across the following paragraph about
- * the ternary expression in the official Study Guide:
- * Ternary Expression Evaluation
- * As of Java 7, only one of the right-hand expressions of the ternary operator will be evaluated at runtime.
- * In a manner similar to the short-circuit operators, if one of the two right-hand expressions in a ternary
- * operator performs a side effect, then it may not be applied at runtime.
- * (See <a href="https://stackoverflow.com/q/29557106/1410078">Stack Overflow Question.</a>)
- * <p/>
- * This is not true! The ternary operator has not changed behavior!
+ * Test-cases for the ternary operator.
  *
  * @author <a href="mailto:jhunovis@gmail.com">Jan Hackel</a>
  * @version $Revision$ $Date$ $Author$
  */
+@SuppressWarnings("ConstantConditionalExpression")
 public class TernaryOperatorJava7Test {
 
     private int testNumber;
 
+    /**
+     * Preparing for the Oracle Certified Associate Java SE 8 Programmer 1 exam, I came across the following paragraph about
+     * the ternary expression in the official Study Guide:
+     * Ternary Expression Evaluation
+     * As of Java 7, only one of the right-hand expressions of the ternary operator will be evaluated at runtime.
+     * In a manner similar to the short-circuit operators, if one of the two right-hand expressions in a ternary
+     * operator performs a side effect, then it may not be applied at runtime.
+     * (See <a href="https://stackoverflow.com/q/29557106/1410078">Stack Overflow Question.</a>)
+     * <p/>
+     * This is not true! The ternary operator has not changed behavior!
+    */
     @Test
     public void testLazyEvaluation_SinceJava7OnlyOneSideShouldEvaluate_1() throws Exception {
         System.out.println(true ? setTestNumberFieldSideBySideEffect(10) : setTestNumberFieldSideBySideEffect(20));
@@ -36,5 +40,14 @@ public class TernaryOperatorJava7Test {
     private int setTestNumberFieldSideBySideEffect(int value) {
         System.out.println("Setting testNumber=" + value);
         return testNumber = value;
+    }
+
+    /**
+     * This works because {@link java.io.PrintStream#println(Object)} will be used in the following example!
+     */
+    @Test
+    public void testDynamicOverloadingSelection() throws Exception {
+        System.out.println(true ? 21 : "Zebra");
+        System.out.println(false ? 21 : "Zebra");
     }
 }
